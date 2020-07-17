fd44a6b8654545f37e52b9ed7a2fbbf708431459
BuildTools <unconfigured@null.spigotmc.org>
2020-07-17 17:43:46 +0300

    setting up favicon ico

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-const express = require('express')
 const path = require('path')
+const express = require('express')
+const favicon = require('serve-favicon')
 const PORT = process.env.PORT || 8000
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
+    .use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('index', { page: 'main' }))
     .get('/docs/*', (req, res) => res.sendFile(req.url))
-    .get('*', (req, res) => res.render('index', { page: req.url }))
+    .get('/', (req, res) => res.render('index', { page: 'main' }))
+    .get('*.ejs', (req, res) => res.render('index', { page: req.url }))
     .listen(PORT)
+
 
 if (PORT == 8000)
     require('opn')
